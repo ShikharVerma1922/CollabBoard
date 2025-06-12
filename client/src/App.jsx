7eb0e96d45bbfe5a8a9d977d32b7ec1ef70912ff
Shikhar Verma <ShikharVerma1922@users.noreply.github.com>
2025-06-12 20:49:12 +0530

    Adding client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-green-900 bg-amber-500 dark:bg-pink-400">
+        Hello world
+      </h1>
+    </>
+  );
+}
+
+export default App;
